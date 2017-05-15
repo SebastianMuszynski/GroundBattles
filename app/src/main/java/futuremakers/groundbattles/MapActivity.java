492c9de8583b6ed2f5e8b0b39d48c4a6f5b53254
Sebastian Muszynski <muszynski.sebastian@gmail.com>
2017-05-15 23:57:22 +0200

    Add hypertrack start/stop tracking

--- app/src/main/java/futuremakers/groundbattles/MapActivity.java
+++ app/src/main/java/futuremakers/groundbattles/MapActivity.java
@@ -1,15 +1,25 @@
 package futuremakers.groundbattles;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
+import com.hypertrack.lib.HyperTrack;
 import com.hypertrack.lib.HyperTrackMapFragment;
 import com.hypertrack.lib.MapFragmentCallback;
+import com.hypertrack.lib.callbacks.HyperTrackCallback;
+import com.hypertrack.lib.models.ErrorResponse;
+import com.hypertrack.lib.models.SuccessResponse;
 
 public class MapActivity extends AppCompatActivity {
+
+    private Button startTrackingBtn;
+    private Button stopTrackingBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +31,32 @@ public class MapActivity extends AppCompatActivity {
 
     private void initView() {
         initToolbar();
+        initBtns();
         initHyperTrackMap();
     }
 
     private void initToolbar() {
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
+    }
+
+    private void initBtns() {
+        startTrackingBtn = (Button) findViewById(R.id.startTrackingBtn);
+        stopTrackingBtn = (Button) findViewById(R.id.stopTrackingBtn);
+
+        startTrackingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startTrackingUser();
+            }
+        });
+
+        stopTrackingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopTrackingUser();
+            }
+        });
     }
 
     private void initHyperTrackMap() {
@@ -40,4 +70,22 @@ public class MapActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "On Map Ready callback", Toast.LENGTH_SHORT).show();
         }
     };
+
+    private void startTrackingUser() {
+        HyperTrack.startTracking(new HyperTrackCallback() {
+            @Override
+            public void onSuccess(@NonNull SuccessResponse successResponse) {
+                Toast.makeText(MapActivity.this, "Started tracking!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(@NonNull ErrorResponse errorResponse) {
+                Toast.makeText(MapActivity.this, "Stopped tracking!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void stopTrackingUser() {
+        HyperTrack.stopTracking();
+    }
 }
