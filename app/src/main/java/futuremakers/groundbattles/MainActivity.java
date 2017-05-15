1e05e86797657848eb343a0f4ce841d92afff73f
Mateusz Staniuk <staniuk.mateusz@gmail.com>
2017-05-15 21:10:53 +0200

    Add google user login

--- app/src/main/java/futuremakers/groundbattles/MainActivity.java
+++ app/src/main/java/futuremakers/groundbattles/MainActivity.java
@@ -1,13 +1,106 @@
 package futuremakers.groundbattles;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity implements
+        GoogleApiClient.OnConnectionFailedListener,
+        View.OnClickListener {
+    private static final String TAG = "MainActivity";
+    private static final int RC_SIGN_IN = 9001;
+    private GoogleSignInOptions googleSignInOptions;
+    private GoogleApiClient googleApiClient;
+    private SignInButton googleSignInButton;
+    private GoogleSignInAccount googleAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getGoogleSignInOptions();
+        getGoogleApiClient();
+
+        googleSignInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        googleSignInButton.setOnClickListener(this);
+    }
+
+    private void getGoogleSignInOptions() {
+        // Configure sign-in to request the user's ID, email address, and basic
+        googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+    }
+
+    private void getGoogleApiClient() {
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+        // options specified by googleSignInOptions.
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, googleSignInOptions)
+                .build();
+    }
+
+    private void handleGoogleSignIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            handleSignInSuccess(result);
+        } else {
+            handleSignInError(result);
+        }
+    }
+
+    private void handleSignInSuccess(GoogleSignInResult result) {
+        googleAccount = result.getSignInAccount();
+        // TODO: Do something useful after signing in
+        Toast.makeText(getApplicationContext(), "Google user signed in", Toast.LENGTH_SHORT).show();
+
+    }
+
+    private void handleSignInError(GoogleSignInResult result) {
+        Toast.makeText(getApplicationContext(), "Google sign in error", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(getApplicationContext(), "Google connection failed", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.sign_in_button:
+                handleGoogleSignIn();
+            break;
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
     }
 }
